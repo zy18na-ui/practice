84de4ad25561aa9f2994d2ab661d9a42979fdaef
zy18na-ui <zy18na@gmail.com>
2025-09-24 08:51:50 +0800

    Delete Services directory

--- Services/IChatLlm.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Capcap.Services
-{
-    public interface IChatLlm
-    {
-        Task<string> ChatAsync(string system, string user, CancellationToken ct);
-        Task<string> ClassifyAsync(string system, string user, CancellationToken ct);
-    }
-}
